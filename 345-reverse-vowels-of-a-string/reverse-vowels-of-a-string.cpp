0b29cca8be4a4c86490376d204b45f4bffbb6a8f
Muhammad Junaid Shaukat <154750865+Junaid-Shaukat@users.noreply.github.com>
2024-06-02 19:57:55 +0500

    Time: 13 ms (8.11%) | Memory: 10 MB (14.01%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -5,15 +5,15 @@ public:
         int left = 0, right = s.size() - 1;
 
         while (left < right) {
-            // Move left pointer to the next vowel
+
             while (left < right && vowels.find(s[left]) == vowels.end()) {
                 left++;
             }
-            // Move right pointer to the previous vowel
+
             while (left < right && vowels.find(s[right]) == vowels.end()) {
                 right--;
             }
-            // Swap the vowels
+
             if (left < right) {
                 std::swap(s[left], s[right]);
                 left++;
