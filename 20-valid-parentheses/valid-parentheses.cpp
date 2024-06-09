1c452fb66663e8c483a614c0a2c92da7a506f43a
Muhammad Junaid Shaukat <154750865+Junaid-Shaukat@users.noreply.github.com>
2024-06-09 15:34:50 +0500

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+    
+    for(char c : s) {
+        if(c == '(' || c == '{' || c == '[') {
+            stack.push(c);
+        } else {
+            if(stack.empty()) return false;
+            
+            char top = stack.top();
+            stack.pop();
+            
+            if((c == ')' && top != '(') ||
+               (c == '}' && top != '{') ||
+               (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.empty();
+    }
+};
