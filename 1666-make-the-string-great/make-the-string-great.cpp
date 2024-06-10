dbf7ffd97181150557543f58c0deeb0dcad4e135
Muhammad Junaid Shaukat <154750865+Junaid-Shaukat@users.noreply.github.com>
2024-06-10 12:24:16 +0500

    Time: 5 ms (27.45%) | Memory: 8.6 MB (21.82%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeGood(string s) {
+            stack<char> stack;
+    
+    for (char c : s) {
+        if (!stack.empty() && 
+            ((stack.top() == c + 32) || (stack.top() == c - 32))) {
+            stack.pop();
+        } else {
+            stack.push(c);
+        }
+    }
+    
+   string result = "";
+    while (!stack.empty()) {
+        result = stack.top() + result;
+        stack.pop();
+    }
+    
+    return result;
+    }
+};
