5a17648facd67a47ca98da80253af1ec9e2bb651
Muhammad Junaid Shaukat <154750865+Junaid-Shaukat@users.noreply.github.com>
2024-07-10 17:13:34 +0500

    Time: 89 ms (10.69%) | Memory: 70.2 MB (67.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
   int maxSubArray(vector<int>& nums) {
-    int max_sum = nums[0]; // Initialize with first element
+    int max_sum = nums[0]; 
     int curr_sum = 0;
 
-    for (int i = 0; i < nums.size(); i++) { // Use nums.size()
-      curr_sum = std::max(nums[i], curr_sum + nums[i]);  // Update curr_sum
-      max_sum = std::max(max_sum, curr_sum);          // Update max_sum
+    for (int i = 0; i < nums.size(); i++) { 
+      curr_sum = std::max(nums[i], curr_sum + nums[i]);  
+      max_sum = std::max(max_sum, curr_sum);          
     }
     return max_sum;
   }
