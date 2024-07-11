799de28cbce9e7ed81d92a370fc11981ca79a115
Muhammad Junaid Shaukat <154750865+Junaid-Shaukat@users.noreply.github.com>
2024-07-11 18:27:29 +0500

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.90%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+          unordered_set<int> seen;
+        
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+
+private:
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            totalSum += digit * digit;
+        }
+        return totalSum;
+    }
+    };
